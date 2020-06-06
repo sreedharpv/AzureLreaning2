7fa5e2b2a492d2802f6b505e754e7fb06fad5ca6
sreedharpv <37132016+sreedharpv@users.noreply.github.com>
2020-06-06 13:49:15 +0100

    Update Application.java

--- app/src/main/java/hello/Application.java
+++ app/src/main/java/hello/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+    }  
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
